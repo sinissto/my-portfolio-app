85f197915501acee2331642532616a621a3a417d
Sinisa <sinisha.stojanovic@gmail.com>
2025-02-16 17:17:35 +0100

    Tailwind font family, colours...

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,10 +17,16 @@ export default {
 			lg: '960px',
 			xl: '1200px',
 		},
+		fontFamily: {
+				primary: 'var(--font-jetBrainsMono)',
+		},
   	extend: {
   		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
+  			primary: '#1c1c22',
+				accent: {
+					DEFAULT: '#00ff99',
+					hover: '#00e187'
+				}
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
